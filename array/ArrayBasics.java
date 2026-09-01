a4c95cc65e7b2eaa9dc6219c455c8ead9507a52d
MdTanwer <tanw9004167@gmail.com>
2026-09-01 05:25:39 +0000

    all issue fixed

---
+++ array/ArrayBasics.java
@@ -0,0 +1,7 @@
+public static void main(String[] args) {
+    int[] array = new int[]{1, 2, 3, 4, 5};
+
+    for (int i = 0; i < array.length; i++) {
+        System.err.println("Array element at index " + i + " is " + array[i]);
+    }
+}
